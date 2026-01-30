b6579121814058ecd7d9b82d281aabd223c294d4
eliasoliman <elia.soliman@studenti.unitn.it>
2026-01-30 11:49:32 +0100

    lil

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {
+      '/genera': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
 })
